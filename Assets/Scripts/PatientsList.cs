069b5a433bfab6ce1997db2d841a03f926e894bd
Bisher Dokkmak <35769802+Bisher-d790@users.noreply.github.com>
2022-12-29 00:38:43 +0700

    [Fix] Fix setting the UI to work with multiple Aspect Ratios and Resolutions, and fix resizing the lists.

--- Assets/Scripts/PatientsList.cs
+++ Assets/Scripts/PatientsList.cs
@@ -7,36 +7,50 @@ public class PatientsList : MonoBehaviour
     [Header("References")]
     [SerializeField] private RectTransform listTransform;
 
-    [Header("UI")]
-    [SerializeField] private float patientsYspaceInBetween = 10.0f;
-
     private List<Patient> patients = new List<Patient>();
 
 
     public void AddPatientToList(Patient patient)
     {
-        float lastPatientYPos = -(((RectTransform)patient.transform).rect.height + patientsYspaceInBetween) * listTransform.childCount;
-
-        // Space for the first item
-        lastPatientYPos -= ((RectTransform)patient.transform).rect.height;
+        float lastPatientYPos = -(((RectTransform)patient.transform).rect.height) * listTransform.childCount;
 
         patient.transform.SetParent(listTransform);
         patient.transform.localPosition = new Vector2(0, lastPatientYPos);
+        patient.transform.localScale = Vector2.one;
 
         patients.Add(patient);
         patient.ParentList = this;
+
+        RefreshContainerListSize();
     }
+
     public void RemovePatientFromList(Patient patient)
     {
         patient.transform.SetParent(transform.parent);
 
         patients.Remove(patient);
         patient.ParentList = null;
+
+        RefreshContainerListSize();
     }
 
     public bool ContainsPatient(Patient patient)
     {
         return patients.Contains(patient);
+    }
+
+    private void RefreshContainerListSize()
+    {
+        // Get the last patient inside the list
+        RectTransform lastPatient = (RectTransform)patients[patients.Count - 1].transform;
+
+        if (lastPatient == null) return;
+
+        // Get the last patients pos and add the height
+        float height = lastPatient.rect.height + Mathf.Abs(lastPatient.localPosition.y);
+
+        // Resize the list
+        listTransform.sizeDelta = new Vector2(listTransform.sizeDelta.x, height);
     }
 
     public IEnumerator<Patient> GetEnumerator() { return patients.GetEnumerator(); }
